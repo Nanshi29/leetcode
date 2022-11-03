455957542909c41377289e5cef94924df872f007
Nanshi kumari <90635009+Nanshi29@users.noreply.github.com>
2022-11-03 18:51:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.43%) - LeetHub

--- 0504-base-7/0504-base-7.cpp
+++ 0504-base-7/0504-base-7.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string convertToBase7(int num) {
-       int rem,ans=0,i=0;
+        int rem,ans=0,i=0;
         while(num!=0)
         {
             rem=num%7;
-            ans += rem * pow(10,i);
+            ans+=rem*pow(10,i);
             i++;
             num=num/7;
         }
