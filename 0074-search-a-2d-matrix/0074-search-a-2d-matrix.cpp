f6ce678b42de5d11092c5f5ac87e439311613904
Nanshi kumari <90635009+Nanshi29@users.noreply.github.com>
2022-11-22 20:09:40 +0530

    Time: 11 ms (33.72%), Space: 9.6 MB (5.04%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int j = 0 ;
+    
+    for(int i = 0 ; i < matrix.size() ; i++)
+    {
+        if(find(matrix[i].begin(), matrix[i].end(),target)!=matrix[i].end())
+        {
+            return true;
+        }
+
+    }
+    return false;
+    }
+};
