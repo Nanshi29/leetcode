c5ed04c3f2c994814eb692d4b9d121fd57f034ad
Nanshi kumari <90635009+Nanshi29@users.noreply.github.com>
2022-12-18 23:41:59 +0530

    Time: 468 ms (39.37%), Space: 24.3 MB (95.36%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int count=0;
+        unordered_map<int,int>m;
+        for(auto &it1:nums1)
+            for(auto &it2:nums2)
+                m[it1+it2]++;
+        for(auto &it3:nums3)
+            for(auto &it4:nums4)
+                if(m.count(0-it3-it4)) count+=m[0-it3-it4];
+        return count;
+                
+        
+    }
+};
