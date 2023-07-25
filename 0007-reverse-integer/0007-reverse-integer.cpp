ce4b121ad4cefc0688e9727138b35bb57dbaa3f6
Nanshi kumari <90635009+Nanshi29@users.noreply.github.com>
2023-07-25 18:40:31 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (14.00%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        int ans=0;
-        while(x!=0) {
-            int d=x%10;
-            if((ans>INT_MAX/10)||(ans<INT_MIN/10))
-            {
-                return 0;
-            }
-            ans=(ans*10)+d;
-            x=x/10;
-        }
-        return ans;
-        
+       long r=0;
+       while(x)
+       {
+          int d=x%10;
+           r=r*10+d;
+           x=x/10;
+       }
+        if(r>INT_MAX || r<INT_MIN)
+            return 0;
+        return r;
     }
 };
