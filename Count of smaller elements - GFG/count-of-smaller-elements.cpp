e0d1954a2dc91e368bf90b3b6d3505cff61c2f17
Nanshi Kumari <90635009+Nanshi29@users.noreply.github.com>
2023-09-11 22:47:48 +0530

    Added solution - LeetHub

--- Count of smaller elements - GFG/count-of-smaller-elements.cpp
+++ Count of smaller elements - GFG/count-of-smaller-elements.cpp
@@ -28,7 +28,11 @@ int countOfElements(int arr[], int n, int x)
     for(int i=0;i<n;i++)
     {
         if(arr[i]<=x)
-        c++;
+        {
+            c++;
+        }
+        
     }
     return c;
+    
 }
